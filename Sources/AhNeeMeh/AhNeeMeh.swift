238f5edc2c375a063f00f0189217893b7e7ef36a
Elijah Yap <yap@usc.edu>
2021-04-07 15:35:47 -0700

    Update AhNeeMeh.swift

--- Sources/AhNeeMeh/AhNeeMeh.swift
+++ Sources/AhNeeMeh/AhNeeMeh.swift
@@ -1,3 +1,20 @@
+import CoreGraphics
+
 struct AhNeeMeh {
     var text = "Hello, World!"
+}
+
+extension CGPoint {
+    static func -(lhs: CGPoint, rhs: CGPoint) -> CGPoint {
+        CGPoint(x: lhs.x-rhs.x, y: lhs.y-rhs.y)
+    }
+    static func +(lhs: CGPoint, rhs: CGPoint) -> CGPoint {
+        CGPoint(x: lhs.x+rhs.x, y: lhs.y+rhs.y)
+    }
+    static func -=(lhs: inout CGPoint, rhs: CGPoint) {
+        lhs = lhs - rhs
+    }
+    static func +=(lhs: inout CGPoint, rhs: CGPoint) {
+        lhs = lhs + rhs
+    }
 }
